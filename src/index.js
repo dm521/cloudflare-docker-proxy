8ebbe2dffff2dda661edb561a4c596428cc039b2
kunkun <kevin@moyutech.cn>
2024-07-15 22:24:43 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.qianjiang.team": dockerHub,
+  "quay.qianjiang.team": "https://quay.io",
+  "gcr.qianjiang.team": "https://gcr.io",
+  "k8s-gcr.qianjiang.team": "https://k8s.gcr.io",
+  "k8s.qianjiang.team": "https://registry.k8s.io",
+  "ghcr.qianjiang.team": "https://ghcr.io",
+  "cloudsmith.qianjiang.team": "https://docker.cloudsmith.io",
+  "ecr.qianjiang.team": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
